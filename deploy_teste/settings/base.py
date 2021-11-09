8ec2d3469659832826d60784cb01dedf4385968d
LucasdeFreitasND <lucas.freitas@neuraldata.com.br>
2021-11-09 14:29:35 -0300

    add o secure-hamlet-08842.herokuapp.com ao allowed_host

--- deploy_teste/settings/base.py
+++ deploy_teste/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-stu8&7p&0sjz--n2$lusotq*3=a5u#h@0si*(y#uy^-#macj!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secure-hamlet-08842.herokuapp.com']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
